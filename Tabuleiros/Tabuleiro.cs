9218a2ed18a5ef66ddfc4bfc5caf10960c1e8b3e
andsGomes <anderson.stos.gomes@gmail.com>
2023-03-23 15:26:38 -0300

    Adding methods for position validation

--- Tabuleiros/Tabuleiro.cs
+++ Tabuleiros/Tabuleiro.cs
@@ -12,15 +12,39 @@ namespace Tabuleiros
       _pecas = new Peca[_linhas, _colunas];
     }
     // This piece method accesses the private piece property of an array. 
-    public Peca peca(int linha, int coluna)
+    public Peca peca(int linhas, int colunas)
     {
-      return _pecas[linha, coluna];
+      return _pecas[linhas, colunas];
+    }
+    public Peca peca(Posicao pos)
+    {
+      return _pecas[pos._linhas, pos._colunas];
+    }
+    // Check if there os a given position
+    public bool existePeca(Posicao pos)
+    {
+       validarPosicao(pos);
+       return peca(pos) != null;
     }
     // Operation to place a piece on the board 
     public void colocarPeca(Peca p, Posicao pos)
     {
       _pecas[pos._linhas, pos._colunas] = p;
       p._posicao = pos;
+    }
+    // Method to test if the position is empty
+    public bool posicaoValida(Posicao pos)
+    {
+      if (pos._linhas < 0 || pos._linhas >= _linhas || pos._colunas < 0 || pos._colunas >= _colunas)
+        return false;
+
+      return true;
+    }
+    // Method to validate position
+    public void validarPosicao(Posicao pos)
+    {
+      if (!posicaoValida(pos))
+        throw new TabuleiroException("Posicao invalida");
     }
   }
 }
---
+++ Tabuleiros/TabuleiroException.cs
@@ -0,0 +1,7 @@
+namespace Tabuleiros
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg){}
+    }
+}
